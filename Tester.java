eb6d4bd7b69b9c380d13f7dd37f8357b0c35fa1f
with-yahya-farooqui <84168130+with-yahya-farooqui@users.noreply.github.com>
2022-02-25 12:22:02 +0530

    Create Tester.java

---
+++ Tester.java
@@ -0,0 +1,67 @@
+class Participant {
+    private static int counter;
+    static {
+        counter = 1001;
+    }
+    private String registrationId, name, branch;
+    private long contactNumber;
+
+    Participant(String name, long contactNumber, String branch) {
+        this.name = name;
+        this.contactNumber = contactNumber;
+        this.branch = branch;
+        registrationId = "D" + counter;
+        counter++;
+    }
+
+    String getRegistrationId() {
+        return registrationId;
+    }
+
+    int getCounter() {
+        return counter;
+    }
+
+    String getName() {
+        return name;
+    }
+
+    long getContactNumber() {
+        return contactNumber;
+    }
+
+    String getBranch() {
+        return branch;
+    }
+
+    void setCounter(int counter) {
+        Participant.counter = counter;
+    }
+
+    void setName(String name) {
+        this.name = name;
+    }
+
+    void setBranch(String branch) {
+        this.branch = branch;
+    }
+
+    void setContactNumber(long contactNumber) {
+        this.contactNumber = contactNumber;
+    }
+}
+
+class Tester {
+    public static void main(String args[]) {
+        Participant p1 = new Participant("Rohit", 1234565789L, "Computer");
+        Participant p2 = new Participant("Sayli", 9999999999L, "Mechanical");
+        Participant p3 = new Participant("Alice", 9999888877L, "Production");
+        Participant p4 = new Participant("Bob", 8888888888L, "Chemical");
+        Participant[] arr = { p1, p2, p3, p4 };
+        for (Participant p : arr) {
+            System.out.println("Hi " + p.getName() + "! Your Registration ID is " + p.getRegistrationId()
+                    + " and branch is " + p.getBranch());
+        }
+    }
+
+}
